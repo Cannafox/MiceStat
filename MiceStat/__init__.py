80595f2596de777080520ed2853cc2daf1372966
Cannafox <gklodkowski@gmail.com>
2024-03-17 22:45:07 +0100

    fixes

---
+++ MiceStat/__init__.py
@@ -0,0 +1,1 @@
+from .micestat import MiceStat
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import MiceStat
 
+# WINDOW_TITLE =
+
 def main():
     miceStat = MiceStat.MiceStat()
+
+    # window = miceStat.initalize_window(WINDOW_TITLE)
 
 if __name__ == "__main__":
     main()
